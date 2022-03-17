f8002b6fa183b4244bba6ebfb1fcff7803f7ff51
Giorgi Beridze <Giorgi.Beridze@space.ge>
2022-03-17 18:40:14 +0400

    remove constants of database name, version, table name

--- app/src/main/java/com/example/movieapp/util/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.example.movieapp.util
-
-object Constants {
-    const val DATABASE_TABLE_NAME = "movies"
-    const val DATABASE_NAME = "movies.db"
-    const val DATABASE_VERSION = 1
-}
