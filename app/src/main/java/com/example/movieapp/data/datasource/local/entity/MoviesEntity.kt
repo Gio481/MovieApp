b7427ee0d34aaa204f09d0500f16dcf21629211d
Giorgi Beridze <Giorgi.Beridze@space.ge>
2022-03-17 15:38:06 +0400

    removed primary key from entity

--- app/src/main/java/com/example/movieapp/data/datasource/local/entity/MoviesEntity.kt
+++ app/src/main/java/com/example/movieapp/data/datasource/local/entity/MoviesEntity.kt
@@ -1,11 +1,10 @@
 package com.example.movieapp.data.datasource.local.entity
 
 import androidx.room.Entity
-import androidx.room.PrimaryKey
 import com.example.movieapp.util.Constants.DATABASE_TABLE_NAME
 
 @Entity(tableName = DATABASE_TABLE_NAME)
 data class MoviesEntity(
-    @PrimaryKey(autoGenerate = false) val id: Long? = null,
+    val id: Long? = null,
     val poster: String,
 )
