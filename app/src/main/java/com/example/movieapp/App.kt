762aabeaffdb37f5f78d07647707c6f35d10d068
Giorgi Beridze <Giorgi.Beridze@space.ge>
2022-03-21 15:06:12 +0400

    create repository pattern

--- app/src/main/java/com/example/movieapp/App.kt
+++ app/src/main/java/com/example/movieapp/App.kt
@@ -3,19 +3,22 @@ package com.example.movieapp
 import android.app.Application
 import com.example.movieapp.di.databaseModule
 import com.example.movieapp.di.networkModule
+import com.example.movieapp.di.repositoryModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
-import org.koin.dsl.module
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
             androidContext(this@App)
-            module {
-                networkModule
-                databaseModule
-            }
+            modules(
+                listOf(
+                    networkModule,
+                    databaseModule,
+                    repositoryModule
+                )
+            )
         }
     }
 }
---
+++ app/src/main/java/com/example/movieapp/domain/repository/movies/MoviesRepository.kt
@@ -0,0 +1,11 @@
+package com.example.movieapp.domain.repository.movies
+
+import com.example.movieapp.domain.model.FavouriteMoviesDomain
+import com.example.movieapp.domain.model.MoviesDomain
+import com.example.movieapp.util.Resources
+
+interface MoviesRepository {
+    suspend fun getTopRatedMovies(page: Int): Resources<List<MoviesDomain>>
+    suspend fun getPopularMovies(page: Int): Resources<List<MoviesDomain>>
+    suspend fun getFavouriteMovies(): List<FavouriteMoviesDomain>
+}
