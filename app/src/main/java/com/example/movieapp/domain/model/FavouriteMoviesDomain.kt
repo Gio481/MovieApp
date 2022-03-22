1d2196de5f6f2a7fffc27d0b138f8da6d56fa52a
Giorgi Beridze <Giorgi.Beridze@space.ge>
2022-03-23 02:31:15 +0400

    create favorite movies adapter

--- app/src/main/java/com/example/movieapp/domain/model/FavouriteMoviesDomain.kt
+++ app/src/main/java/com/example/movieapp/domain/model/FavouriteMoviesDomain.kt
@@ -1,8 +1,12 @@
 package com.example.movieapp.domain.model
 
 data class FavouriteMoviesDomain(
-    val id:Long,
-    val poster:String,
-    val releaseDate:String,
-    val rating:Double
-)
+    val id: Long,
+    val poster: String,
+    val releaseDate: String,
+    val rating: Double
+) {
+    fun rating(): String {
+        return rating.toString()
+    }
+}
