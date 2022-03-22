76cf497e51e7c99b6d42ebd927254e687e80d878
Giorgi Beridze <Giorgi.Beridze@space.ge>
2022-03-23 01:55:07 +0400

    change movies use case function return type, function configuration

--- app/src/main/java/com/example/movieapp/domain/interactor/usecases/get_movies/GetMoviesUseCase.kt
+++ app/src/main/java/com/example/movieapp/domain/interactor/usecases/get_movies/GetMoviesUseCase.kt
@@ -1,7 +1,12 @@
 package com.example.movieapp.domain.interactor.usecases.get_movies
 
+import androidx.paging.PagingData
+import com.example.movieapp.domain.model.MoviesDomain
+import com.example.movieapp.util.Resources
+import kotlinx.coroutines.flow.Flow
+
 interface GetMoviesUseCase {
-    suspend fun getPopularMovies(page:Int)
-    suspend fun getTopRatedMovies(page:Int)
+    suspend fun getPopularMovies(): Resources<Flow<PagingData<MoviesDomain>>>
+    suspend fun getTopRatedMovies(): Resources<Flow<PagingData<MoviesDomain>>>
     suspend fun getFavouriteMovies()
 }
