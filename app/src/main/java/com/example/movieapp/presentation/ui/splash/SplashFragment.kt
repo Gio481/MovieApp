d26cf4333a7bb9c9f719372bd81b41fbe2e97412
Giorgi Beridze <Giorgi.Beridze@space.ge>
2022-03-24 01:40:37 +0400

    configure splash screen

--- app/src/main/java/com/example/movieapp/presentation/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/example/movieapp/presentation/ui/splash/SplashFragment.kt
@@ -2,15 +2,20 @@ package com.example.movieapp.presentation.ui.splash
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.example.movieapp.R
 import com.example.movieapp.databinding.FragmentSplashBinding
 import com.example.movieapp.presentation.base.BaseFragment
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SplashFragment : BaseFragment<FragmentSplashBinding, SplashScreenViewModel>() {
     override val bindingInflater: (inflater: LayoutInflater, container: ViewGroup?, attachToRoot: Boolean) -> FragmentSplashBinding
         get() = FragmentSplashBinding::inflate
 
-    override val viewModel: SplashScreenViewModel by viewModels()
+    override val viewModel: SplashScreenViewModel by viewModel()
 
-    override fun onBindViewModel(viewModel: SplashScreenViewModel) {}
+    override fun onBindViewModel(viewModel: SplashScreenViewModel) {
+        viewModel.splashScreenDelay()
+        findNavController().navigate(R.id.action_splashFragment_to_moviesCollectionFragment)
+    }
 }
--- app/src/main/java/com/example/movieapp/presentation/ui/splash/SplashScreenViewModel.kt
+++ app/src/main/java/com/example/movieapp/presentation/ui/splash/SplashScreenViewModel.kt
@@ -1,5 +1,18 @@
 package com.example.movieapp.presentation.ui.splash
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-class SplashScreenViewModel:ViewModel() {}
+class SplashScreenViewModel : ViewModel() {
+    fun splashScreenDelay() {
+        viewModelScope.launch {
+            delay(DELAY)
+        }
+    }
+
+    companion object {
+        private const val DELAY = 1000L
+    }
+}
