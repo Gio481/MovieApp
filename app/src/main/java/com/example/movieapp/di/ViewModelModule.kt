ade0a223cc3be39802bb092661553b67d34b684d
Giorgi <Gio.beridze33@gmail.com>
2022-03-28 15:43:13 +0400

    remove splash screen view model

--- app/src/main/java/com/example/movieapp/di/ViewModelModule.kt
+++ app/src/main/java/com/example/movieapp/di/ViewModelModule.kt
@@ -3,12 +3,10 @@ package com.example.movieapp.di
 import com.example.movieapp.presentation.ui.favourite_movies.viewmodel.FavouriteMoviesViewModel
 import com.example.movieapp.presentation.ui.movie_details.viewmodel.MovieDetailsViewModel
 import com.example.movieapp.presentation.ui.movies.viewmodel.MoviesViewModel
-import com.example.movieapp.presentation.ui.splash.viewmodel.SplashScreenViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
-    viewModel { SplashScreenViewModel() }
     viewModel { MoviesViewModel(get()) }
     viewModel { FavouriteMoviesViewModel(get()) }
     viewModel { MovieDetailsViewModel(get()) }
