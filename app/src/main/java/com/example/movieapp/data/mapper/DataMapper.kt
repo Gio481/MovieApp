a11e9b594f1c93b2404b01c6a9892a9a8e734a69
Giorgi <Gio.beridze33@gmail.com>
2022-04-04 15:33:12 +0400

    change mapper class configuration

--- app/src/main/java/com/example/movieapp/data/mapper/DataMapper.kt
+++ app/src/main/java/com/example/movieapp/data/mapper/DataMapper.kt
@@ -1,7 +1,7 @@
 package com.example.movieapp.data.mapper
 
 interface DataMapper<in DTO, DOMAIN, ENTITY> {
-    fun dtoToDomain(dto: DTO): List<DOMAIN>
-    fun fromEntity(entity: List<ENTITY>): List<DOMAIN>
+    fun dtoToDomain(dto: DTO): DOMAIN
+    fun fromEntity(entity: ENTITY): DOMAIN
     fun toEntity(favoriteMovie: DOMAIN): ENTITY
 }
