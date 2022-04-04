d9ebe67984176caae9dac8ea33137591587ca541
Giorgi <Gio.beridze33@gmail.com>
2022-04-04 15:51:36 +0400

    change getAllMovies function returned type with PagingSource

--- app/src/main/java/com/example/movieapp/data/datasource/local/dao/MoviesDao.kt
+++ app/src/main/java/com/example/movieapp/data/datasource/local/dao/MoviesDao.kt
@@ -1,5 +1,6 @@
 package com.example.movieapp.data.datasource.local.dao
 
+import androidx.paging.PagingSource
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
@@ -11,12 +12,12 @@ interface MoviesDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(movie: MoviesEntity)
 
-    @Query("SELECT id FROM movies")
-    suspend fun getAllMoviesID(): List<Int>
+    @Query("SELECT posterPath FROM movies")
+    suspend fun getAllMoviesID(): List<String>
 
     @Query("SELECT * FROM movies")
-    suspend fun getAllMovies(): List<MoviesEntity>
+    fun getAllMovies(): PagingSource<Int, MoviesEntity>
 
-    @Query("DELETE FROM movies WHERE id=:id")
-    suspend fun deleteMovie(id: Int)
+    @Query("DELETE FROM movies WHERE posterPath=:posterPath")
+    suspend fun deleteMovie(posterPath: String)
 }
