6a11472f29867a7353e23b00f5769f7c03b03b5a
Giorgi <Gio.beridze33@gmail.com>
2022-04-06 19:09:57 +0400

    change mapper class configuration

--- app/src/main/java/com/example/movieapp/data/mapper/DataMapperImpl.kt
+++ app/src/main/java/com/example/movieapp/data/mapper/DataMapperImpl.kt
@@ -1,33 +1,37 @@
 package com.example.movieapp.data.mapper
 
 import com.example.movieapp.data.datasource.local.entity.MoviesEntity
-import com.example.movieapp.data.datasource.remote.dto.MoviesDTO
+import com.example.movieapp.data.datasource.remote.dto.MoviesResponseDTO
 import com.example.movieapp.domain.model.MoviesDomain
 
-class DataMapperImpl : DataMapper<MoviesDTO, MoviesDomain, MoviesEntity> {
+class DataMapperImpl : DataMapper<MoviesResponseDTO, MoviesDomain, MoviesEntity> {
 
-    override fun dtoToDomain(dto: MoviesDTO): MoviesDomain {
-        return MoviesDomain(
-            id = dto.id,
-            title = dto.title,
-            posterPath = dto.posterPath,
-            originalTitle = dto.originalTitle,
-            overview = dto.overview,
-            rating = dto.voteAverage,
-            releaseDate = dto.releaseDate
-        )
+    override fun dtoToDomain(dto: MoviesResponseDTO): List<MoviesDomain> {
+        return dto.results.map {
+            MoviesDomain(
+                id = it.id,
+                title = it.title,
+                posterPath = it.posterPath,
+                originalTitle = it.originalTitle,
+                overview = it.overview,
+                rating = it.voteAverage,
+                releaseDate = it.releaseDate
+            )
+        }
     }
 
-    override fun fromEntity(entity: MoviesEntity): MoviesDomain {
-        return MoviesDomain(
-            id = entity.id,
-            posterPath = entity.posterPath,
-            releaseDate = entity.releaseDate,
-            rating = entity.rating,
-            overview = entity.overview,
-            title = entity.title,
-            originalTitle = entity.originalTitle
-        )
+    override fun fromEntity(entity: List<MoviesEntity>): List<MoviesDomain> {
+        return entity.map {
+            MoviesDomain(
+                id = it.id,
+                posterPath = it.posterPath,
+                releaseDate = it.releaseDate,
+                rating = it.rating,
+                overview = it.overview,
+                title = it.title,
+                originalTitle = it.originalTitle
+            )
+        }
     }
 
     override fun toEntity(favoriteMovie: MoviesDomain): MoviesEntity {
